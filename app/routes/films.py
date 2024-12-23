20144f0e4c85f4e864918aebe4fe4527d47f6701
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-12-23 18:41:57 +0100

    change film end point implementation, scusa paso ;(

--- app/routes/films.py
+++ app/routes/films.py
@@ -6,7 +6,7 @@ films_bp = Blueprint("films", __name__)
 
 
 
-@films_bp.route("", methods=["GET"])
+@films_bp.route("/", methods=["GET"])
 def get_films():
     films = list(mongo.db.films.find())
     # Convertire il campo `_id` in stringa
@@ -14,7 +14,7 @@ def get_films():
         film["_id"] = str(film["_id"])
     return jsonify(films), 200
 
-@films_bp.route("", methods=["POST"])
+@films_bp.route("/", methods=["POST"])
 def add_film():
 
     data = request.json
@@ -26,46 +26,34 @@ def add_film():
 
 
 @films_bp.route('/<int:content_id>', methods=["GET"])
-def get_film(content_id):
-    films = list(mongo.db.films.find())
-
-    # Cerca un film per ID
-    film = next((c for c in films if c['id'] == content_id), None)
-    if not film:
-        abort(404, description="Content not found")
-    film["_id"] = str(film["_id"])
-    return jsonify(film)
+def get_film(filmId):
+    film = mongo.db.actors.find_one({"filmId": filmId})
+    if film:
+        film["_id"] = str(film["_id"])
+        return jsonify(film), 200
+    return jsonify({"error": "film not found"}), 404
 
 
 @films_bp.route('/<int:content_id>', methods=["PUT"])
-def update_film(content_id):
-    films = list(mongo.db.films.find())
-    film = next((c for c in films if c['id'] == content_id), None)
+def update_film(filmId):
     data = request.json
-    valid, error = validate_film(data)
-    if not valid:
-        return jsonify(error), 400
-
-    # Aggiorna i campi del film esistente
-    mongo.db.films.update_one(
-        {"_id": content_id},
-        {"$set": data}  # Aggiorna solo i campi forniti
+    updated_film = mongo.db.actors.find_one_and_update(
+        {"filmId": filmId},
+        {"$set": data},
+        return_document=True
     )
-
-    return jsonify(film), 200
+    if updated_film:
+        updated_film["_id"] = str(updated_film["_id"])
+        return jsonify(updated_film), 200
+    return jsonify({"error": "Film not found"}), 404
 
 
 @films_bp.route('/<int:content_id>', methods=["DELETE"])
-def delete_film(content_id):
-    films = list(mongo.db.films.find())
-    film = next((c for c in films if c['id'] == content_id), None)
-    if not film:
-        abort(404, description="Content not found")
-
-    # Rimuove il film dalla lista
-    mongo.db.films.delete_one(film)
-    return jsonify({"message": f"Film with id {content_id} deleted successfully"}), 200
-
+def delete_film(filmId):
+    result = mongo.db.actors.delete_one({"filmId": filmId})
+    if result.deleted_count > 0:
+        return "", 204
+    return jsonify({"error": "filmId not found"}), 404
 
 
 
